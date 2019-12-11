948e34e9251eff9af6091552bdb8efa7fe0d8f97
Emily Martins <emily.flakeheart@gmail.com>
2019-12-11 08:19:48 +0100

    day11 [wip]: working p1 / p2

---
+++ Day11/haskell/src/Common.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Common
+    ( SolutionF
+    , Solution(..)
+    , runSolution
+    )
+  where
+
+import Control.Monad.Writer
+import Text.Printf
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+type SolutionF s = T.Text -> WriterT [String] IO s
+
+data Solution a b = MkSolution { day   :: Int -- Day number
+                               , part1 :: SolutionF a
+                               , part2 :: SolutionF b
+                               , testSpec :: IO ()
+                               }
+
+colorRed, colorGreen, colorCyan, colorReset :: String
+colorRed = "\x1b[31m"
+colorGreen = "\x1b[32m"
+colorCyan = "\x1b[36m"
+colorReset = "\x1b[0m"
+
+runSolution :: (Show a, Show b) => Solution a b -> FilePath -> IO ()
+runSolution MkSolution{day, part1, part2, testSpec} fp = do
+    printf "--- Running Advent of Code Solution for %sDay %02d%s --- \n" colorCyan day colorReset
+    printf "%s> Tests%s\n" colorGreen colorReset
+    testSpec
+    contents <- T.readFile fp
+    printf "%s> Part 1%s\n" colorGreen colorReset
+    printf "%s<%s\n" colorRed colorReset
+    (p1res, p1log) <- runWriterT (part1 contents)
+    mapM_ (putStrLn . ("Info: "++)) p1log
+    putStrLn . ("Result: "++) . show $ p1res
+    printf "%s> Part 2%s\n" colorGreen colorReset
+    printf "%s<%s\n" colorRed colorReset
+    (p2res, p2log) <- runWriterT (part2 contents)
+    mapM_ (putStrLn . ("Info: "++)) p2log
+    putStrLn . ("Result: "++) . show $ p2res
+    printf "--- End --- \n"
---
+++ Day11/haskell/src/Solution.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Solution where
+
+import qualified Data.Text as T
+import Common
+import Language.IntCode
+import Test.Hspec
+import Data.List.Split
+import Data.List
+import Control.Monad.Writer
+import qualified Data.Map as Map
+import Data.Map (Map)
+import qualified Data.Set as Set
+import Data.Set (Set)
+import Linear
+import Debug.Trace
+import Control.Lens
+import Data.Ord
+import qualified Graphics.Image as I
+
+
+type World = Map (V2 Int) Int
+
+data WorldState = WS { _world       :: World
+                     , _position    :: V2 Int
+                     , _direction   :: V2 Int
+                     , _outputStack :: [Integer]
+                     , _paintCount  :: Integer
+                     , _prevStates  :: [World]
+                     }
+                     deriving Show
+
+makeLenses ''WorldState         
+                 
+rotateLeft, rotateRight :: V2 Int -> V2 Int
+rotateLeft (V2 x y) = V2 (negate y) x 
+rotateRight (V2 x y) = V2 y (negate x)
+
+startState :: WorldState
+startState = WS { _world       = Map.empty
+                , _position    = V2 0 0
+                , _direction   = V2 0 1
+                , _outputStack = []
+                , _paintCount  = 0
+                , _prevStates  = []
+                }
+
+machineInterpPaint :: Step -> WorldState -> WorldState
+machineInterpPaint (Step ms)      ws = 
+    case ws ^. outputStack of
+        (dir:color:rest) ->
+            let newDirection = (if dir == 0 then rotateLeft else rotateRight) (ws ^. direction)
+                newPosition  = ws ^. position ^+^ newDirection
+                newState = ws & direction .~ newDirection
+                              & position .~ newPosition
+                              & outputStack .~ rest
+                              & world %~ (Map.insert (ws ^. position) (fromIntegral color))
+                              & prevStates %~ (ws^.world :)
+            in machineInterpPaint (stepMachine ms) (newState)
+        _ -> machineInterpPaint (stepMachine ms) ws
+machineInterpPaint (StepInp f)    ws = 
+    case (ws ^.world) Map.!? (ws ^. position) of
+        Nothing -> machineInterpPaint (stepMachine (f (fromIntegral 0))) ws
+        Just c  -> machineInterpPaint (stepMachine (f (fromIntegral c))) ws
+machineInterpPaint (StepOut v ms) ws = machineInterpPaint (stepMachine ms) (ws & outputStack %~ (v:))
+machineInterpPaint StepHalt       ws = ws
+
+
+one :: SolutionF Int
+one t = do
+    let instructions :: [Integer] = fmap read . splitOn "," . T.unpack $ t
+        machine = makeMachine instructions
+        steps = stepMachine machine
+        worldState = machineInterpPaint steps startState 
+    pure . Map.size $ worldState ^. world
+
+two :: SolutionF Int
+two t = do
+    let instructions :: [Integer] = fmap read . splitOn "," . T.unpack $ t
+        machine = makeMachine instructions
+        steps = stepMachine machine
+        worldState = machineInterpPaint steps (startState & world .~ Map.fromList [(V2 0 0, 1)])
+        minx = view _x $ minimumBy (comparing (view _x)) $ Map.keys (worldState ^. world)
+        miny = view _y $ minimumBy (comparing (view _y)) $ Map.keys (worldState ^. world)
+        maxx = view _x $ maximumBy (comparing (view _x)) $ Map.keys (worldState ^. world)
+        maxy = view _y $ maximumBy (comparing (view _y)) $ Map.keys (worldState ^. world)
+        width = maxx - minx
+        height = maxy - miny
+
+    forM_ (zip [0..100] (reverse $ worldState ^. prevStates)) $ \(i, st) -> do    
+        liftIO $ print i
+        let res = I.makeImageR I.VU (height * 10, width * 10) (\(y, x) -> case (st) Map.!? (V2 (x - 50) (negate (y - 50))) of
+                Nothing -> I.PixelY 0.1
+                Just 1 -> I.PixelY 1.0
+                Just _ -> I.PixelY 0)        
+        liftIO $ I.writeImage ("out/image" ++ show i ++ ".png") (res :: I.Image I.VU I.Y Double)
+
+    tell [show (minx, miny, maxx, maxy)]
+    tell [show (width, height)]
+    pure 2
+
+
+tests :: IO ()
+tests = hspec $ pure ()
+
+solution :: Solution Int Int
+solution = MkSolution { day = 11
+                      , part1 = one
+                      , part2 = two
+                      , testSpec = tests
+                      }
