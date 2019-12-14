0a91e6672f6563f1c381bb64b0ae3600b0d36198
Emily Martins <emily.flakeheart@gmail.com>
2019-12-14 09:34:35 +0100

    day14: finished

--- Day14/haskell/src/Solution.hs
+++ Day14/haskell/src/Solution.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Solution where
 
@@ -27,16 +28,21 @@ import Text.Parsec
 import Debug.Trace
 import Text.Parsec.String
 
-data Requirement = Branch (Integer, String) [(Integer, Requirement)]
-                 | Ore Integer
-                 deriving (Show, Eq)
-
 data Rule = Rule { _depend  :: [(Integer, String)]
                  , _produce :: (Integer, String)
                  }             
                  deriving Show
 
 makeLenses ''Rule
+
+data Reaction = Reaction { _reactants :: [(Integer, String)]
+                         , _productAmount :: Integer
+                         }
+                         deriving Show
+
+makeLenses ''Reaction             
+
+type Tome = Map String Reaction
 
 type Resources = Map String Integer
 
@@ -57,50 +63,40 @@ parseRule = do
 parseRules :: Parser [Rule]
 parseRules = parseRule `sepBy` char '\n'
 
-convertRequirement :: [Rule] -> Requirement
-convertRequirement = go (1, "FUEL")
-    where go :: (Integer, String) -> [Rule] -> Requirement
-          go (n, "ORE") _ = Ore n
-          go (n, tag) rs = case find (\Rule{_produce=(_, name)} -> name == tag) rs of
-            Just (Rule deps prod) -> Branch prod $ fmap (\(i, v) -> (i, go (i, v) rs)) deps
-            Nothing -> error "???"
+constructTome :: [Rule] -> Tome
+constructTome = Map.fromList . fmap (\Rule {..} -> (snd _produce, Reaction _depend (fst _produce)))
 
-                            -- let go' = do
-                            --     have <- (\m -> fromMaybe 0 $ m Map.!? name) <$> ST.get
-                            --     traceShowM ("Have", have, name)
-                            --     if have < amt then do
-                            --         traceShowM ("Didn't have enough", name)
-                            --         ix name -= res
-                            --         go v
-                            --     else do
-                            --         traceShowM ("Did have enough", name)
-                            --         ix name -= res
-                            --         pure 0
-                            -- in go'
+data Stack = Stack { _res :: Resources
+                   , _tome :: Tome
+                   }
+                   deriving Show
 
-resStore :: String -> Integer -> ST.State Resources ()
-resStore k v = ST.modify (\m -> Map.insertWith (+) k v m)
+makeLenses ''Stack
 
-findCosts :: Requirement -> ST.State Resources Integer
-findCosts req = go req
-    where go :: Requirement -> ST.State Resources Integer
-          go (Branch (prodAmt, prodName) requires) = do
-               vals <- mapM (\(need, req) -> do
-                    case req of
-                        Branch (produces, name) _ -> do
-                            have <- (fromMaybe 0 . (Map.!? name)) <$> ST.get
-                            if need > have then do
-                                ret <- replicateM (fromIntegral $ 1 + (need - 1 - have) `div` produces) (go req)
-                                resStore name (negate need)
-                                pure . sum $ ret
-                            else do
-                                resStore name (negate need)
-                                pure 0
-                        Ore v -> pure v) requires
-               resStore prodName prodAmt
-               have <- (fromMaybe 0 . (Map.!? prodName)) <$> ST.get
-               pure . sum $ vals
-          go (Ore integer) = pure integer
+resStore :: String -> Integer -> ST.State Stack ()
+resStore k v = ST.modify (\m -> m & res %~ Map.insertWith (+) k v)
+
+ceilDiv :: Integer -> Integer -> Integer
+ceilDiv x y = (x + y - 1) `div` y
+
+oreCostFor :: String -> Integer -> ST.State Stack Integer
+oreCostFor "ORE" amount = pure amount
+oreCostFor key   amount = do
+    res' <- use res
+    tome' <- use tome
+    let have = fromMaybe 0 $ res' Map.!? key
+        reaction = tome' Map.! key
+    if have > amount then do
+        resStore key (negate amount)
+        pure 0
+    else do
+        let wanted = amount - have
+            remainder = wanted `mod` reaction ^. productAmount
+            left = if remainder == 0 then 0 else reaction ^. productAmount - remainder
+            mult = wanted `ceilDiv` (reaction ^. productAmount)
+        res %= Map.insert key left
+        vals <- mapM (\(a, k) -> oreCostFor k (a * mult)) (reaction ^. reactants)
+        pure . sum $ vals
 
 one :: SolutionF Integer
 one t = do
@@ -108,10 +104,19 @@ one t = do
     case rules of 
         Left e -> error (show e)
         Right rs -> do
-            let reqs = convertRequirement rs
-            let startState = Map.empty
+            let tome = constructTome rs
+            let test :: String -> Integer -> Integer
+                test k v = ST.evalState (oreCostFor k v) (Stack Map.empty tome)
+            pure $ test "FUEL" 1
 
-            pure $ ST.evalState (findCosts reqs) startState 
+binarySearch :: (Integer, Integer) -> (Integer -> Integer) -> Integer ->Integer    
+binarySearch (low, high) f target =
+    let middle = low + ((high - low) `div` 2)
+        found = f middle
+    in case found `compare` target of
+        _ | low + 1 == high -> middle
+        GT -> binarySearch (low, middle) f target
+        LT -> binarySearch (middle, high) f target
 
 two :: SolutionF Integer
 two t = do
@@ -119,15 +124,10 @@ two t = do
     case rules of 
         Left e -> error (show e)
         Right rs -> do
-            let reqs = convertRequirement rs
-            let startState = Map.empty
-
-            let test :: Integer -> Integer
-                test n = 
-                    let (Branch (i, s) d) = reqs
-                    in ST.evalState (findCosts (Branch (n, s) d)) Map.empty
-
-            pure $ test 3
+            let tome = constructTome rs
+            let test :: String -> Integer -> Integer
+                test k v = ST.evalState (oreCostFor k v) (Stack Map.empty tome)
+            pure $ binarySearch (0, 10 ^ 10) (test "FUEL") 1000000000000
 
 tests :: IO ()
 tests = hspec $ pure ()
